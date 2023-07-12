b150753c532d191c7d78f3a59c8513c4483dd683
Angiegit config --global user.name 'Angie <adeschutter6437@gmail.com>
2023-07-12 07:22:42 -0700

    edit get

--- controllers/destinations_controller.js
+++ controllers/destinations_controller.js
@@ -6,8 +6,8 @@ const { Destination } = db
 const { createClient } =require( '@supabase/supabase-js')
 const DATABASE_URL = 'https://kzpuwykecupbyqdjibud.supabase.co'
 const DATABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imt6cHV3eWtlY3VwYnlxZGppYnVkIiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODg4NTU3NjgsImV4cCI6MjAwNDQzMTc2OH0.oeGSvX55KS0rCby8Ip1ZdSn5rkkswIOZLVySftlpyqo"
+import supabase from'../supabase'
 
-const supabase = createClient(DATABASE_URL,DATABASE_KEY);
 
 
 // FIND ALL DESTINATIONS
@@ -30,7 +30,8 @@ destinations.get('/:name', async (req, res) => {
         .from("desintations")
         .select()
         .is("name", request.params.name)
-        res.status(200).json(foundDestination)
+
+        res.send(foundDestination)
     } catch (Error) {
         console.log(Error)
         res.status(500).send('Oh no, could not find destination')
