307c4306c326e6cc533660b9c654ee0913999fbd
Angiegit config --global user.name 'Angie <adeschutter6437@gmail.com>
2023-07-12 17:54:21 -0700

    changed get

--- controllers/destinations_controller.js
+++ controllers/destinations_controller.js
@@ -13,7 +13,7 @@ destinations.get('/', async (req, res) => {
         let { data: destinations, error } = await supabase
             .from('destinations')
             .select()
-        console.log(destinations)
+            res.status(200).json(destinations)
         
     } catch (error) {
         console.log(error)
@@ -28,7 +28,7 @@ destinations.get('/:name', async (req, res) => {
         let { data: destinations, error } = await supabase
             .from('destinations')
             .select('name')
-        console.log(destinations)
+            res.status(200).json(destinations)
        
     } catch (Error) {
         console.log(Error)
